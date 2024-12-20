6eb1e47d911e0b3078eff40b7279783d8aec5e37
ゆだ <yuchannel2017@gmail.com>
2024-12-20 16:11:13 +0900

    テスト用にvst.pyでウィンドウが出るようにした

--- module/vst.py
+++ module/vst.py
@@ -1,1 +1,22 @@
 import dawdreamer
+
+import PySide6
+from PySide6.QtWidgets import (QApplication, QWidget)
+import os
+import sys
+
+class TestWindow(QWidget):              # ウィンドウ系クラスを継承すること
+    def __init__(self, parent=None):    # parentは他にウィンドウを表示させる場合に指定する
+        super().__init__(parent)        # 継承元クラス（ここではQWidget）を初期化
+
+
+if __name__ == "__main__":
+    # 環境変数にPySide6を登録
+    dirname = os.path.dirname(PySide6.__file__)
+    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+    
+    app = QApplication(sys.argv)    # PySide6の実行
+    window = TestWindow()           # ユーザがコーディングしたクラス
+    window.show()                   # PySide6のウィンドウを表示
+    sys.exit(app.exec())            # PySide6の終了
