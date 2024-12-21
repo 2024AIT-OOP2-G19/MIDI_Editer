7a4f2b9bb0f61caacaddb06eef7b687657a1535d
aibisu765 <118408290+aibisu765@users.noreply.github.com>
2024-12-21 23:04:38 +0900

    Updata_midi_rw.py
    「midi_rw.py」の方で新規作成と読み込みと書き込みの操作はできた
    
    テスト用のプログラムで実行したから大丈夫だと思います

--- module/midi_rw.py
+++ module/midi_rw.py
@@ -1,1 +1,18 @@
-import mido
+from mido import MidiFile, MidiTrack, Message
+
+def create_midi(file_name: str):
+    midi = MidiFile()
+    track = MidiTrack()
+    midi.tracks.append(track)
+    track.append(Message('program_change', program=12, time=0))
+    midi.save(file_name)
+    print(f"New MIDI file created: {file_name}")
+
+def load_midi(file_path: str) -> MidiFile:
+    midi = MidiFile(file_path)
+    print(f"MIDI file loaded: {file_path}")
+    return midi
+
+def save_midi(midi: MidiFile, file_path: str):
+    midi.save(file_path)
+    print(f"MIDI file saved: {file_path}")
