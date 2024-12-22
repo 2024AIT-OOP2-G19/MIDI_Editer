23f66de240e34ac1454c70bf1f01b5b151bdcd32
ゆだ <yuchannel2017@gmail.com>
2024-12-23 01:59:26 +0900

    テスト用ウィンドウにボタン表示

--- module/vst.py
+++ module/vst.py
@@ -1,14 +1,30 @@
 import dawdreamer
 
-import PySide6
-from PySide6.QtWidgets import (QApplication, QWidget)
-
-class TestWindow(QWidget):              # ウィンドウ系クラスを継承すること
-    def __init__(self, parent=None):    # parentは他にウィンドウを表示させる場合に指定する
-        super().__init__(parent)        # 継承元クラス（ここではQWidget）を初期化
+from PySide6.QtWidgets import (QApplication, QWidget, QPushButton)
+'''
+テスト用クラス
+'''
+class TestWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
         self.setWindowTitle('vstTest')
+        self.resize(200, 110)
 
+        load_vst_btn = QPushButton(self)
+        load_vst_btn.move(20, 10)
+        load_vst_btn.setText('load vst')
+        # load_vst_btn.pressed.connect()
+
+        vst_option_btn = QPushButton(self)
+        vst_option_btn.move(20, 40)
+        vst_option_btn.setText('vst option')
+        # vst_option_btn.pressed.connect()
+
+        render_btn = QPushButton(self)
+        render_btn.move(20, 70)
+        render_btn.setText('render')
+        # render_btn.pressed.connect()
 
 if __name__ == "__main__":
     app = QApplication()
