10b6df19006e042d8da25bcc5d3adb2fa923fc89
ゆだ <yuchannel2017@gmail.com>
2024-12-23 14:02:10 +0900

    add: load_vst

--- module/vst.py
+++ module/vst.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import (QApplication, QWidget, QPushButton)
+from PySide6.QtWidgets import (QApplication, QWidget, QPushButton, QFileDialog)
 
 import dawdreamer as daw
 import numpy as np
@@ -8,9 +8,23 @@ from scipy.io import wavfile
 class Vst():
     def __init__(self, sample_rate=44100, buffer_size=128):
 
-        self.vst_path = "/Library/Audio/Plug-Ins/VST3/Serum.vst3"
+        self.vst_path = '/Library/Audio/Plug-Ins/VST3/Serum.vst3'
         self.engine = daw.RenderEngine(sample_rate, buffer_size)
-        self.plugin = self.engine.make_plugin_processor("Serum", self.vst_path)
+        self.plugin = self.engine.make_plugin_processor('plugin', self.vst_path)
+
+    def loadVst(self):
+        file,check = QFileDialog.getOpenFileName(None, "ファイルを選択してください。","/Library/Audio/Plug-Ins","All Files (*);;vst Files (*.vst);;vst3 Files (*.vst3)")
+
+        if check:
+            print(f'check: {check}')
+            self.vst_path = file
+
+        print(f'vst path: {self.vst_path}')
+
+        self.plugin = self.engine.make_plugin_processor('plugin', self.vst_path)
+
+
+        
 
     def vstEditer(self):
         print('open vst editer')
@@ -33,7 +47,7 @@ class TestWindow(QWidget):
         load_vst_btn = QPushButton(self)
         load_vst_btn.move(20, 10)
         load_vst_btn.setText('load vst')
-        # load_vst_btn.pressed.connect()
+        load_vst_btn.pressed.connect(lambda: vst.loadVst())
 
         vst_editer_btn = QPushButton(self)
         vst_editer_btn.move(20, 40)
