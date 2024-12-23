1398a3b5442faf2efc1adf2926eca518c2b820e8
ゆだ <yuchannel2017@gmail.com>
2024-12-23 12:39:53 +0900

    Vstクラスの初期化とopenEditer関数

--- module/vst.py
+++ module/vst.py
@@ -1,12 +1,31 @@
-import dawdreamer
-
 from PySide6.QtWidgets import (QApplication, QWidget, QPushButton)
+
+import dawdreamer as daw
+import numpy as np
+from scipy.io import wavfile
+
+
+class Vst():
+    def __init__(self, sample_rate=44100, buffer_size=128):
+
+        self.vst_path = "/Library/Audio/Plug-Ins/VST3/Serum.vst3"
+        self.engine = daw.RenderEngine(sample_rate, buffer_size)
+        self.plugin = self.engine.make_plugin_processor("Serum", self.vst_path)
+
+    def vstEditer(self):
+        print('open vst editer')
+        self.plugin.open_editor()
+
+    
+
 '''
-テスト用クラス
+テスト用ウィンドウ
 '''
 class TestWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        vst = Vst()
 
         self.setWindowTitle('vstTest')
         self.resize(200, 110)
@@ -16,10 +35,10 @@ class TestWindow(QWidget):
         load_vst_btn.setText('load vst')
         # load_vst_btn.pressed.connect()
 
-        vst_option_btn = QPushButton(self)
-        vst_option_btn.move(20, 40)
-        vst_option_btn.setText('vst option')
-        # vst_option_btn.pressed.connect()
+        vst_editer_btn = QPushButton(self)
+        vst_editer_btn.move(20, 40)
+        vst_editer_btn.setText('vst editer')
+        vst_editer_btn.pressed.connect(lambda: vst.vstEditer())
 
         render_btn = QPushButton(self)
         render_btn.move(20, 70)
